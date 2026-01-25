cc79bb87da9ea91cfd69ab9ae44041e622e6552e
Luidson Lima <luidsonlsti@gmail.com>
2026-01-25 19:27:02 -0300

    feat: add eager loading to profile pic

--- app/components/atoms/Avatar.tsx
+++ app/components/atoms/Avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Avatar({ src = "/avatar.svg", alt = "Avatar", size = 96 }: { src?: string; alt?: string; size?: number }) {
   return (
     <div className="w-auto">
-      <Image src={src} alt={alt} width={size} height={size} className="rounded-full" />
+      <Image src={src} alt={alt} width={size} height={size} loading="eager" className="rounded-full" />
     </div>
   )
 }

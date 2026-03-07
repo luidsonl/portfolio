abd41bbd220acf86b94d10ea42b081c5b8ca2f9a
Luidson Lima <luidsonlsti@gmail.com>
2026-03-07 19:02:49 -0300

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,21 +6,21 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const res = await fetch("https://api.github.com/users/luidsonl/repos");
   const repos = await res.json();
 
-  const projects = repos.filter((r: any) => r.has_pages);
+  const projects = repos.filter((r: any) => r.has_pages && r.name !== "luidsonl.github.io");
 
   const pages = projects.map((repo: any) => ({
     url: `https://luidsonl.github.io/${repo.name}`,
-    lastModified: new Date(repo.updated_at),
+    lastModified: new Date(repo.updated_at).toISOString(),
   }));
 
   return [
     {
       url: "https://luidsonl.github.io/",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: "https://luidsonl.github.io/en",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     ...pages,
   ];

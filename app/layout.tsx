336f42d2317411d0f30a489f252894f2fa752b83
Luidson Lima <luidsonlsti@gmail.com>
2026-03-07 18:04:47 -0300

    update google verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   description: "Desenvolvedor Full-Stack | Node.js, Next.js, PHP, Laravel",
 
   verification: {
-    google: "tb2ErqgEeTKaRqlHB-1d0wn2evE1dUDKs1oK0EMNwYg",
+    google: "LFWW5_Q9YkGKqJ5b9BNMfvXshaTZZd9Fj4cUm8kEjQU",
   },
 
   alternates: {
